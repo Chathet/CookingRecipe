d82eb6fb050fc0b60465707ba9adcb955433a349
Chathet <www.wiktor.bochenski@gmail.com>
2021-08-24 11:51:21 +0200

    Database connection added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const bodyparser = require('body-parser')
 const path = require('path')
+const mongodb = require('mongodb')
+
+const mongoClient = mongodb.MongoClient
 
 const app = express()
 const port = 3000
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'CookingRecipe'
+let userName
+let userAge
+let operation
 
 app.use(express.static(path.join(__dirname, '/../public')))
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
-let userName
-let userAge
+
+
+mongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client)=>{
+if(error)
+{
+  return console.log('Unable to connect to database!')
+}})
+
+
 
 app.get('/', (req, res) => {
   if(!userName){
@@ -30,15 +45,33 @@ app.post('/',(req, res) => {
   
   userName = req.body.name
   userAge = req.body.age
+  operation = 'add'
   res.send("Name: " + userName + " , " + "Age: " + userAge)
 })
 
 
 app.delete('/',(req, res) => {
-  userName = undefined
-  userAge = undefined
+  userName = res.body.name
+  userAge = req.body.age
+  operation = 'delete'
   res.send("Deleted")
 })
+
+if(operation === 'add')
+{
+  db.collection('users').insertOne({
+    name: userName,
+    age: userAge
+}, (error,result) => {
+    if(error)
+    {
+        return console.log('Unable to insert user')
+    }
+
+    console.log(result.insertedId)
+})
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
